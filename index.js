9b8e4dc7d43f3ad80131a0f60f8ad10241aadbb6
Nicolas Marini <nmarini44@gmail.com>
2019-04-28 19:53:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+  const firstTwoDrivers = function(array){ array.map{return array[0] || array[1]}}
+  return firstTwoDrivers(array)
+}
